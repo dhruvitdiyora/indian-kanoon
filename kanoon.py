1e67b3dfbbdd1f6faa7a845c334b3de6de9ce2e6
dhruvit.diyora <dhruvit.diyora@webosphere.in>
2023-08-19 16:20:52 +0530

    modified for loops

--- kanoon.py
+++ kanoon.py
@@ -1,11 +1,26 @@
 from requests.structures import CaseInsensitiveDict # request CaseInsensitiveDicts
-import cfscrape # scrape clouflare websites
-
-from bs4 import BeautifulSoup as bs # for parsing html
+import os
+import warnings
+import sys
 from pathlib import Path # for creating directories
 import time
 
-import asyncio # for asynchronous process
+
+try: 
+    import cfscrape # scrape clouflare websites
+except (ModuleNotFoundError, ImportError):
+    print("cfscrape module not found")
+    os.system(f"{sys.executable} -m pip install -U cfscrape")
+finally:
+    import cfscrape
+
+try:
+    from bs4 import BeautifulSoup as bs
+except (ModuleNotFoundError, ImportError):
+    print("BeautifulSoup module not found")
+    os.system(f"{sys.executable} -m pip install -U beautifulsoup4")
+finally:
+    from bs4 import BeautifulSoup as bs
 
 
 scraper = cfscrape.create_scraper() # create cfscrape object
@@ -41,7 +56,7 @@ def download(url, path, title) : # downloads pdfs from url
         title = title.replace('/', '-')
         filename = Path(path+'/'+title+'.pdf')
         filename.write_bytes(r.content)
-        time.sleep(1) # delay for 1 second to overcome scrapping limit per second
+        time.sleep(0.5) # delay for 1 second to overcome scrapping limit per second
     except Exception as e:
         print(f"Error while downloading {url}: {e}")
 
@@ -61,6 +76,7 @@ def results(court, id, year, month, s): # fetches doc links from search results
 
 def months(court, id, year, s) : # fetches links for each month
     try:
+        data = []
         months, months_url = [], []
         for month in s.find_all('a', href=True) :
             months.append(month.string)
@@ -74,13 +90,34 @@ def months(court, id, year, s) : # fetches links for each month
             pages_url = []
             for res in s.find_all('span', attrs={'class':'pagenum'}) :
                 pages_url.append(res.a['href'])
-                
+                data.append(res.a['href'])
+
             for n in range(0, len(pages_url)):
                 url1 = url_home+pages_url[n]
                 s1 = crawler(url1)
                 results(court, id, year, months[i], s1)
+                if n == len(pages_url)-1:
+                    for res1 in s1.find_all('span', attrs={'class':'pagenum'}) :
+                        if res1.a['href'] not in data:
+                            pages_url.append(res1.a['href'])
+                            data.append(res1.a['href'])
+                    scroll_next(pages_url[n:len(pages_url)],court,id,year,months[i],data)
     except Exception as e:
         print(f"Error while processing months: {e}")
+    
+def scroll_next(pages,court,id,year,months,data):
+    for j in range(0, len(pages)-1):
+        url2 = url_home+pages[j]
+        s2 = crawler(url2)
+        results(court, id, year, months, s2)
+        if j == len(pages)-1:
+            for ress1 in s2.find_all('span', attrs={'class':'pagenum'}) :
+                if ress1.a['href'] not in data:
+                    pages.append(ress1.a['href'])
+                    data.append(ress1.a['href'])
+            if len(pages)-1 > j:
+                scroll_next(pages[j+1:len(pages)-1],court,id,year,months,data)
+
 
 def court_years(court, id): # fetches links for each year
     try:
